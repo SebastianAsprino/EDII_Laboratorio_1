a389fdb747f4ce54828547a4ffd203b496869efb
SebastianAsprino <asprinosebastian@proton.me>
2024-03-17 20:18:58 -0500

    modulo nodo que sera usado en el arbol.

--- nodo.py
+++ nodo.py
@@ -1,11 +1,25 @@
 ## Definición del módulo Nodo
 class nodo:
-    """Esta clase crea el nodo el cual es una hoja o rama
-        del arbol.
+  """
+  Definición del módulo Nodo.
+
+  Esta clase crea el nodo que representa una hoja o rama del árbol.
+
+  Atributos:
+    hash: El hash almacena el md5 del archivo.
+    ruta: La ruta del archivo.
+    altura: La altura del nodo en el arbol.
+    izquierda: Referencia al hijo izquierdo del nodo.
+    derecha: Referencia al hijo derecho del nodo.
+  """
+  def __init__(self, ruta, id_hash):
     """
-    def __init__(self, valor):
-        self.valor = valor
-        self.izquierda = None
-        self.derecha = None
-
-
+    Parámetros:
+      ruta: La ruta del archivo en data.
+      hash: El hash del archivo.
+      """
+    self.ruta = ruta
+    self.id_hash = id_hash
+    self.altura = 1
+    self.izquierda = None
+    self.derecha = None
