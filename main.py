9e258a0a327275e5011f1d451b52f2f92749aef5
SebastianAsprino <asprinosebastian@proton.me>
2024-03-17 22:18:21 -0500

    impmementado en main aunque se rompe al agregar un segundo elemento, revisar mañana

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from nodo import nodo
+from arbol import arbol
 
 if __name__ == "__main__":
-    nodo_raiz = nodo(5)
-    print(nodo_raiz.valor)
+    mi_arbol = arbol()
+    mi_arbol.raiz = mi_arbol.insertar(mi_arbol.raiz, "soy una ruta", 100)
+    mi_arbol.raiz = mi_arbol.insertar(mi_arbol.raiz, "soy una ruta1", 101)
+    mi_arbol.raiz = mi_arbol.insertar(mi_arbol.raiz, "soy una ruta2", 99)
+    print(mi_arbol.raiz)
+    print(mi_arbol.raiz.ruta)
+    print(mi_arbol.raiz.id_hash)
+    print(mi_arbol.raiz.altura)
+    print(mi_arbol.raiz.izquierda)
+    print(mi_arbol.raiz.derecha)
+
