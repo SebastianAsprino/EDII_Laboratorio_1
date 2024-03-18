d0a25503c58dec00c9edc54b21217c2ab096cec9
SebastianAsprino <asprinosebastian@proton.me>
2024-03-18 18:13:51 -0500

    y los ordena por nombre de archivo

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from arbol import arbol
 
 if __name__ == "__main__":
     mi_arbol = arbol()
-    mi_arbol.raiz = mi_arbol.insertar(mi_arbol.raiz, "archivo100", "/unaruta", 1)
-    mi_arbol.raiz = mi_arbol.insertar(mi_arbol.raiz, "archivo101", "/unaruta", 2)
-    mi_arbol.raiz = mi_arbol.insertar(mi_arbol.raiz, "archivo99", "/unaruta", 3)
+    mi_arbol.raiz = mi_arbol.insertar(mi_arbol.raiz, "archivo100.bpm", "/unaruta", 1)
+    mi_arbol.raiz = mi_arbol.insertar(mi_arbol.raiz, "archivo101.jpg", "/unaruta", 2)
+    mi_arbol.raiz = mi_arbol.insertar(mi_arbol.raiz, "archivo99.png", "/unaruta", 3)
     print(mi_arbol.raiz)
     print(mi_arbol.raiz.nombre)
     print(mi_arbol.raiz.ruta)
