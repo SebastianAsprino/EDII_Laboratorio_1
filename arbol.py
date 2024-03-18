3e1260baf1b0e9950235b84ba134bdba595ab893
SebastianAsprino <asprinosebastian@proton.me>
2024-03-17 20:25:45 -0500

    documentacion de la clase arbol Documentacion hasta el momento de la creacion de la clase arbol la cual sera en ese laboratorio.

--- arbol.py
+++ arbol.py
@@ -1,5 +1,10 @@
 ## Definición del módulo arbol
 class arbol:
   """
-  
+  Definición del módulo arbol.
+
+  Esta es la clase donde se construye el arbol AVL el cual
+  sera usado para este laboratorio.
+
+  Atributos:
   """
