2336f4dbe652bbff2866655b0319e4c2ddab7876
SebastianAsprino <asprinosebastian@proton.me>
2024-03-18 22:41:19 -0500

    documentacion de hash

--- hash.py
+++ hash.py
@@ -1,11 +1,18 @@
 import hashlib
 
 def generar_hash_archivo(ruta_archivo):
+    """
+    Genera el hash MD5 de un archivo pasado por parametro.
 
+    Parámetros:
+        ruta_archivo: La ruta del archivo para el cual se calculará el hash.
+
+    Retorna:
+        hash_md5: El hash del archivo en formato string.
+    """
     with open(ruta_archivo, "rb") as archivo:
-        print(ruta_archivo)
         contenido = archivo.read()
     
     hash_md5 = hashlib.md5(contenido)
-    print(hash_md5)
+
     return hash_md5.hexdigest()
